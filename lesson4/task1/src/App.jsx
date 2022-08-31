a7a65482aac3a2bbee2b8cc311768bdbc314f217
ichmen <ichmen@rambler.ru>
2022-08-31 11:20:34 +0300

    reset added

--- lesson4/task1/src/App.jsx
+++ lesson4/task1/src/App.jsx
@@ -16,8 +16,8 @@ function counterReducer(state = 0, action) {
   switch (action.type) {
     case INCREMENT:
       return state + 1;
-    case INCREMENT:
-      return state + 1;
+    case RESET:
+      return 0;
     case DECREMENT:
       return state - 1;
 
